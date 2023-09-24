0b702debc594feb5c227443c3f5cb59898967d0d
Pervejhasan <mdpervej12441@gmail.com>
2023-09-24 08:34:20 +0600

    get all users

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { verifyTokenAndAuthorization } = require("./verifyToken.route");
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken.route");
 const UserModel = require("../models/User.model");
 const router = express.Router();
 
@@ -24,5 +24,47 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+//DELETE
+router.delete('/:id',verifyTokenAndAuthorization, async (req,res)=>{
+try {
+    await UserModel.findByIdAndDelete(req.params.id);
+    res.status(200).json('user has been deleted...')
+} catch (error) {
+    res.status(500).json(error)
+}
+
+})
+
+//GET user
+router.get('/find/:id',verifyTokenAndAdmin, async (req,res)=>{
+    try {
+      const user= await UserModel.findById(req.params.id);
+      const {password,...others}=user._doc;
+      res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
+    })
+    
+//GET all users
+
+router.get('/',verifyTokenAndAdmin, async (req,res)=>{
+
+    
+
+    try {
+      const users= await UserModel.find();
+      res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
+    })
+
+
+
+
 
 module.exports = router;
